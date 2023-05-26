37394f6937577f96907a77b145791c56ab3defba
NiklasRulec <niklasrulec.98@gmail.com>
2023-05-26 14:53:31 +0200

    merge

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-// ! ++++++++++ Überprüfen, ob der Browser Local Storage unterstützt ++++++++++
+// ! ++++++++++ ÜBERPRÜFEN; OB BROWSER LOCAL STORAGE UNTERSTÜTZT ++++++++++
 
 if (typeof Storage !== "undefined") {
   // Wenn Local Storage unterstützt wird, die vorhandenen Elemente laden
   if (localStorage.getItem("cityList")) {
-    document.querySelector(".left-list").innerHTML =
+    document.querySelector("#left-list").innerHTML =
       localStorage.getItem("cityList");
   } else {
     alert("Willkommen. Du hast bisher noch keine Städte gespeichert");
@@ -11,6 +11,8 @@ if (typeof Storage !== "undefined") {
 } else {
   alert("Local Storage wird in diesem Browser nicht unterstützt");
 }
+
+// ! ++++++++++ EIGENE POSITION ++++++++++
 
 navigator.geolocation.getCurrentPosition(function (position) {
   let lat = position.coords.latitude;
@@ -25,6 +27,8 @@ navigator.geolocation.getCurrentPosition(function (position) {
       console.log(data);
     });
 });
+
+// ! ++++++++++ STADT EINGEBEN & WETTERDATEN LADEN ++++++++++
 
 let city = document.querySelector("#city");
 
@@ -56,31 +60,53 @@ function wetterCheck() {
           let airPressure = Math.round(data.main.pressure);
           console.log({ airPressure });
 
-          // ! +++++ NEUE CARD ERSTELLEN
+          // - +++++ NEUE CARD ERSTELLEN
           let newCard = document.createElement("article");
-          // ! +++++ ORTSNAMEN EINFÜGEN
+          // - +++++ ORTSNAMEN EINFÜGEN
           let newCityName = document.createElement("h2");
           newCityName.textContent = "Name : " + cityName;
           newCard.appendChild(newCityName);
-          // ! +++++ TEMPERATUR EINFÜGEN
+          // - +++++ TEMPERATUR EINFÜGEN
           let cityTemperature = document.createElement("h2");
           cityTemperature.textContent = "Celsius : " + celsius;
           newCard.appendChild(cityTemperature);
-          // ! +++++ HUMIDITY EINFÜGEN
+          // - +++++ HUMIDITY EINFÜGEN
           let cityHumidity = document.createElement("h2");
           cityHumidity.textContent = "humidity : " + humidity;
           newCard.appendChild(cityHumidity);
-          // ! +++++ PRESSURE EINFÜGEN
+          // - +++++ PRESSURE EINFÜGEN
           let cityPressure = document.createElement("h2");
           cityPressure.textContent = "pressure : " + airPressure;
           newCard.appendChild(cityPressure);
-          // ! +++++ CARD INS DOCUMENT EINFÜGEN
+          // - ++++++++++ DELETE BUTTON EINFÜGEN ++++++++++
+          let deleteButton = document.createElement("button");
+          let deleteText = document.createTextNode("Löschen");
+          deleteButton.appendChild(deleteText);
+          deleteButton.classList.add("deletebutton");
+          newCard.appendChild(deleteButton);
+          // - +++++ CARD INS DOCUMENT EINFÜGEN
           document.querySelector("#left-list").appendChild(newCard);
-          // ! +++++ IN LOCAL STORAGE SPEICHERN
+          // - +++++ IN LOCAL STORAGE SPEICHERN
           localStorage.setItem(
             "cityList",
-            document.querySelector(".left-list").innerHTML
+            document.querySelector("#left-list").innerHTML
           );
+          // - ++++++++++ RELOAD ++++++++++
+          location.reload();
         });
     });
 }
+
+// ! ++++++++++ DELETE BUTTON FUNCTION ++++++++++
+let deleteButtons = document.querySelectorAll(".deletebutton");
+
+deleteButtons.forEach(function (element) {
+  element.addEventListener("click", function () {
+    this.parentElement.remove();
+    localStorage.setItem(
+      "cityList",
+      document.querySelector("#left-list").innerHTML
+    );
+    console.log("Card wurde gelöscht");
+  });
+});
