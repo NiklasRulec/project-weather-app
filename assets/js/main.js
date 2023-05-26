2930588b394b71ea72aadce1bb6f992dfc800afa
NiklasRulec <niklasrulec.98@gmail.com>
2023-05-26 09:46:30 +0200

    h2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ navigator.geolocation.getCurrentPosition(function (position) {
   console.log({ lat }, { lon });
 
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid={a0b3f65f61d0c176e7f5b42fa8744a3b}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a0b3f65f61d0c176e7f5b42fa8744a3b`
   )
     .then((response) => response.json())
     .then((data) => {
