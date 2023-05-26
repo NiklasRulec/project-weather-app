b5d188443742752f9945e2cf52b6d5490cff293e
NiklasRulec <niklasrulec.98@gmail.com>
2023-05-26 10:54:10 +0200

    code

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,21 +12,45 @@ navigator.geolocation.getCurrentPosition(function (position) {
     });
 });
 
-let zipCode = document.querySelector("#zipinput");
-let countryCode = document.querySelector("#country");
+// let zipCode = document.querySelector("#zipinput");
+// let countryCode = document.querySelector("#country");
+
+let city = document.querySelector("#city");
 
 function wetterCheck() {
-  event.preventDefault();
-  let countryValue = countryCode.value.toLowerCase();
-  let zipValue = zipCode.value;
-  console.log(countryValue);
-  console.log(zipValue);
-  
+  // event.preventDefault();
+  // let countryValue = countryCode.value.toLowerCase();
+  // let zipValue = zipCode.value;
+  // console.log(countryValue);
+  // console.log(zipValue);
+
+  // fetch(
+  //   `http://api.openweathermap.org/geo/1.0/zip?zip=${zipValue},${countryValue}&appid=a0b3f65f61d0c176e7f5b42fa8744a3b`
+  // )
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //   });
+
+  let cityName = city.value;
+  console.log(cityName);
   fetch(
-    `http://api.openweathermap.org/geo/1.0/zip?zip=${zipValue},${countryValue}&appid=a0b3f65f61d0c176e7f5b42fa8744a3b`
+    `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=a0b3f65f61d0c176e7f5b42fa8744a3b`
   )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      let cityLat = data[0].lat;
+      let cityLon = data[0].lon;
+      console.log({ cityLat }, { cityLon });
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=a0b3f65f61d0c176e7f5b42fa8744a3b`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          let celsius = (data.main.temp - 32) * (5 / 9);
+          document.querySelector("#temperature").innerHTML = celsius;
+        });
     });
-  }
+}
